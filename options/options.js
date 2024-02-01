8f3f608804a100601649948686ab7ce3c31563cb
Crowdbotics <team@crowdbotics.com>
2024-02-01 19:46:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kaiser-p3-45744",
+  url: "https://kaiser-p3-45744.botics.co",
+  api: "https://kaiser-p3-45744.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
